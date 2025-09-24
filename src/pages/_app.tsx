4d83d8c0dba6975959ca22cc76ecf18c24221a07
Madson Silva <madsonsilva@MacBook-Air-de-Madson.local>
2025-09-24 16:00:40 -0300

    chore: add google analytics config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-8Q1NQQQE6T" />
+    </>
+  );
 }
