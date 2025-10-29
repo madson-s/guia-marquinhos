481ed738ed08031ac7f3057ab4c40ca59a1096e7
VitorDanielA <vdanieldev1@gmail.com>
2025-10-29 12:29:33 -0300

    fix: alteração no icone e title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icone-marquinhos-novo.png" />
+        <title>Guia Marquinhos | Chapada Diamantina</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
