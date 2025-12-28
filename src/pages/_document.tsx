9d7c22d1dc82f9b616274ac3e330159ad29bf4a9
Madson Silva <madsonsilva@MacBook-Air-de-Madson.local>
2025-12-28 16:50:03 -0300

    convert images to webp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         
         {/* Meta tags adicionais */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Guia Marquinhos" />
       </Head>
